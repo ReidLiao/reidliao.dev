6e5524da34bae0e7e77690841ef1492bc3999056
Reid Liao <hi@reidliao.dev>
2026-07-19 02:58:49 +0800

    update

--- app/(main)/vps/page.tsx
+++ app/(main)/vps/page.tsx
@@ -34,7 +34,7 @@ export default function ProjectsPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          自用验证的机房。
+          长期自用的机房
         </h1>
         <p className="mt-6 text-base leading-relaxed text-zinc-600 dark:text-zinc-400">
           <Balancer>
